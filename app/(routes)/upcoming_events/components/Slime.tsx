460583070314419f5a149bcc93581d54ce73695c
James Dimacali <frostbitezebra421@gmail.com>
2023-12-18 18:10:10 -0800

    styles

--- app/(routes)/upcoming_events/components/Slime.tsx
+++ app/(routes)/upcoming_events/components/Slime.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Slime = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="absolute w-full h-[50rem] sm:h-[60rem] top-0 mt-[-200px]  flex items-center text-center justify-center">
+    <div className="absolute w-full h-[48rem] sm:h-[60rem] top-0 mt-[-200px]  flex items-center text-center justify-center">
       <Image src="/slime4.svg" alt="slime" fill className="object-cover" />
       {children}
     </div>
