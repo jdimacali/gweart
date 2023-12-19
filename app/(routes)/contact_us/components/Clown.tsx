89c08a5ece1334a36f760b557f1d1d185bfe7deb
James Dimacali <frostbitezebra421@gmail.com>
2023-12-18 21:04:10 -0800

    updated clown size

--- app/(routes)/contact_us/components/Clown.tsx
+++ app/(routes)/contact_us/components/Clown.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Clown = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex flex-col justify-center items-center lg:overflow-visible overflow-clip">
-      <div className="relative h-80 max-sm:w-[130%] w-[120%] z-2 ">
+      <div className="relative h-80 max-sm:w-[135%] w-[120%] z-2 ">
         <Image
           src="/clown/top.png"
           fill
@@ -12,7 +12,7 @@ const Clown = ({ children }: { children: React.ReactNode }) => {
         />
       </div>
       <div className="flex justify-center items-center w-full">{children}</div>
-      <div className="relative h-60 max-sm:w-[120%] w-[100%] z-2">
+      <div className="relative h-60 max-sm:w-[125%] w-[100%] z-2">
         <Image
           src="/clown/bottom.png"
           fill
