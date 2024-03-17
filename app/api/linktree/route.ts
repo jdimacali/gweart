9e273e85b42ce268be2747bae78fff119f052427
James Dimacali <frostbitezebra421@gmail.com>
2024-03-16 20:58:07 -0700

    updated get linktree api

--- app/api/linktree/route.ts
+++ app/api/linktree/route.ts
@@ -6,7 +6,9 @@ export const dynamic = "force-dynamic";
 
 export async function GET() {
   try {
-    const response = await axios.get(`${API_URL}/api/beacon?populate=*`);
+    const response = await axios.get(
+      `${API_URL}/api/Beacon?populate=Links&populate[0]=Title.Font&populate[1]=Subtitle.Font`
+    );
     return NextResponse.json(response.data.data);
   } catch (error: any) {
     console.error("Linktree", error);
