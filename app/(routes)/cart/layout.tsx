c297ff8f5d57f22c7104985dab8be9273a5a6cc0
James Dimacali <frostbitezebra421@gmail.com>
2024-04-05 14:48:46 -0700

    pushing to production on vercel

--- app/(routes)/cart/layout.tsx
+++ app/(routes)/cart/layout.tsx
@@ -1,8 +1,4 @@
-export default function Layout({
-  children, // will be a page or nested layout
-}: {
-  children: React.ReactNode;
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section className="h-full w-full text-black bg-white items-center justify-center pb-20 px-10 md:px-10 lg:px-20 2xl:px-80">
       {children}
