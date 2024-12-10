a98a2291a3c07ce7b673b8287198db5d172d44f5
James Dimacali <frostbitezebra421@gmail.com>
2024-12-09 17:23:26 -0800

    fixed loading

--- app/(dashboard)/components/SpooktacularGoodies.tsx
+++ app/(dashboard)/components/SpooktacularGoodies.tsx
@@ -49,7 +49,7 @@ const SpooktacularGoodies = () => {
             initial={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.75 }}
-            className="absolute inset-0 flex justify-center items-center bg-gradient-to-b from-violet-900/90 via-violet-900/60 to-violet-950/60 z-50"
+            className="absolute inset-0 flex justify-center items-center bg-gradient-to-b from-violet-900/90 via-violet-900/60 to-violet-950/60 z-20"
           >
             <Spin />
           </motion.div>
